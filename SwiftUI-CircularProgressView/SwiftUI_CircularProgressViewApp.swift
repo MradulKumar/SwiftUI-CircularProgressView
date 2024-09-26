8cadf5ccfd32b888e8a0eca3422f52c2a6471a15
MradulKumar <mradul.iimt@gmail.com>
2024-09-26 08:55:18 +0530

    Initial Commit

---
+++ SwiftUI-CircularProgressView/SwiftUI_CircularProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CircularProgressViewApp.swift
+//  SwiftUI-CircularProgressView
+//
+//  Created by Mradul Kumar on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CircularProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
